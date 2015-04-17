2dc6befa597e06d4a29133598f2ee1607858ce04
George Brassey <gbrassey@gmail.com>
2015-04-16 21:44:11 -0400

    adds UDP!!!

--- src/javascripts/drum-player.js
+++ src/javascripts/drum-player.js
@@ -11,14 +11,20 @@ Drums.forEach(function(drum) {
 	drumPlayer[drum] = audio;
 });
 
-drumPlayer.play = function play (symbol) {
+var playNow = function playNow (symbol) {
 	if (drumPlayer[symbol].paused) {
 		drumPlayer[symbol].play();
-	}else{
-		drumPlayer[symbol].currentTime = 0
+	} else {
+		drumPlayer[symbol].currentTime = 0;
+	}
+};
+
+drumPlayer.play = function play (symbol) {
+	if (drumPlayer[symbol]) {
+		playNow(symbol);
+	} else {
+		playNow(Drums[0]);
 	}
 }
 
-
 module.exports = drumPlayer;
-
